e5d25bf85d6100b979349b0972d187d2cafcb9c0
Jon Williams <jonathan.williams@gmail.com>
2018-04-05 22:32:38 -0400

    envelopes are triggered and the filter works

--- src/SID.cpp
+++ src/SID.cpp
@@ -21,12 +21,14 @@ struct VSid : Module {
 	enum ParamIds {
 		FREQ,
 		WAVEFORM,
+		CUTOFF,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		OSC3_OUT,
 		AUDIO_OUT,
 		NUM_OUTPUTS
 	};
@@ -43,46 +45,54 @@ struct VSid : Module {
 
 	SID sid;
 	long clock = 0;
-	const int base = 0x0e;
 
 };
 
 
 void VSid::onSampleRateChange() {
 	sid.adjust_sampling_frequency(engineGetSampleRate());
-	//sid.enable_filter(true);
+	sid.enable_filter(true);
+	sid.write(0x17,3|(15<<4)); // routing to 3 and resonant
+	sid.write(0x18,15|(1<<4)); // set volume to 15
 
-	sid.write(0x15,2); // cutoff low
-	sid.write(0x16,2); // cutoff high
-	sid.write(0x17,4); // routing to voice 3
-	sid.write(0x18,16); // set volume to 15 + lopass
 
-	sid.write(0x15,15); // filter cutoff
-	sid.write(0x16,3);
+	for(int base = 0; base <= 0x0e; base+=7 ) {
+		sid.write(base+3,4); // pw
+		sid.write(base+4,5);
 
-	// third voice
-	sid.write(base+3,4); // pw
-	sid.write(base+4,5);
-
-	sid.write(base+5,0); // AD
-	sid.write(base+6,5); // SR
+		sid.write(base+5,0); // AD
+		sid.write(base+6,56); // SR
+	}
 
 }
 
 
 void VSid::step() {
 
-	sid.write(base+0,0); // freq
-	sid.write(base+1,127);
-	sid.write(base+1, params[FREQ].value);
-	sid.write(base+4, int(params[WAVEFORM].value)| (clock++ % 10000 == 0 ? 1 : 0)|2); // control register
+	for(int base = 0; base <= 0x0e; base+=7 ) {
+		sid.write(base+0,0); // freq
+		sid.write(base+1, params[FREQ].value);
+		sid.write(base+4, int(params[WAVEFORM].value)| (clock++ % 100000 == 0 ? 1 : 0)); // control register
+	}
+
+
+	sid.write(0x15,0); // cutoff low
+	sid.write(0x16,int(params[CUTOFF].value)); // cutoff high
+
 
 	sid.clock(CLOCK/engineGetSampleRate());
-	sid.enable_filter(false);
-	int16_t sample = sid.output();
-	sample = sid.read(0x1b); // use osc 3
-	float value = 100.f * MAX_VOLTAGE*float(sample)/(float)SHRT_MAX;
-	outputs[AUDIO_OUT].value = value;
+
+	{
+		int16_t sample = sid.read(0x1b); // use osc 3
+		float value = 100.f * MAX_VOLTAGE*float(sample)/(float)SHRT_MAX;
+		outputs[OSC3_OUT].value = value;
+	}
+	{
+		int16_t sample = sid.output(16);
+		float value = MAX_VOLTAGE*float(sample)/(float)SHRT_MAX;;
+		outputs[AUDIO_OUT].value = value;
+	}
+
 }
 
 
@@ -102,9 +112,14 @@ VSidWidget::VSidWidget(VSid *module) : ModuleWidget(module) {
 	addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 0)));
 	addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
 	addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 365)));
-	addOutput(Port::create<PJ301MPort>(Vec(3*RACK_GRID_WIDTH, box.size.y - 2*RACK_GRID_WIDTH), Port::OUTPUT, module, VSid::AUDIO_OUT));
+
 	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, RACK_GRID_WIDTH), module, VSid::FREQ, 0, 255, 0));
-	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, box.size.y - 2*RACK_GRID_WIDTH), module, VSid::WAVEFORM, 0, 255, 0));
+	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, 3*RACK_GRID_WIDTH), module, VSid::WAVEFORM, 0, 255, 0));
+
+	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, 5*RACK_GRID_WIDTH), module, VSid::CUTOFF, 0, 255, 0));
+
+	addOutput(Port::create<PJ301MPort>(Vec(2*RACK_GRID_WIDTH, box.size.y - 4*RACK_GRID_WIDTH), Port::OUTPUT, module, VSid::OSC3_OUT));
+	addOutput(Port::create<PJ301MPort>(Vec(2*RACK_GRID_WIDTH, box.size.y - 2*RACK_GRID_WIDTH), Port::OUTPUT, module, VSid::AUDIO_OUT));
 
 }
 
