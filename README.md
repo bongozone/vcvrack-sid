5a9ef86b3d54a97a0a33e6aa5b1cc0a702aa1db3
Jon Williams <jonathan.williams@gmail.com>
2018-04-19 16:36:22 -0400

    rough panel

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # VCVRack SID
+
+Panel concept:
+```
+______  oct   pw   adsr
+|      |  \   [_]  [_][_][_][_]
+| FREQ | [_]  [_]   / / / / / /
+|______|  ⊚    ⊚   〰 ▮ ◢ ▲ ⃘ 3
+⊚gate ⊚freq  ⊚sync
+
+______  oct   pw   adsr
+|      |  \   [_]  [_][_][_][_]
+| FREQ | [_]  [_]   / / / / / /
+|______|  ⊚    ⊚   〰 ▮ ◢ ▲ ⃘ 1
+⊚gate ⊚freq  ⊚sync
+
+______  oct   pw   adsr
+|      |  \   [_]  [_][_][_][_]
+| FREQ | [_]  [_]   / / / / / /
+|______|  ⊚    ⊚   〰 ▮ ◢ ▲ ⃘ 2
+⊚gate ⊚freq  ⊚sync
+
+______   mode res         \⊚mute3
+|      | \\\  [ ] ex 1 2 3 ⏣env3
+|  cut | [_]  [_]  \ \ \ \ ⏣osc3
+|______|  ⊚    ⊚   ⊚       ⏣audio
+
+```
--- src/SID.cpp
+++ src/SID.cpp
@@ -3,6 +3,15 @@
 #include "sid.h"
 #define MAX_VOLTAGE 5.f
 #define CLOCK 1022730
+
+/*
+TODO:
+	- should gates be normalled?
+	- should freqs be normalled?
+	- pitch not constant across sample rates!
+*/
+
+
 /*
 0 REM *** C64-WIKI SOUND-DEMO ***
 10 S = 54272: W = 17: ON INT(RND(TI)*4)+1 GOTO 12,13,14,15
@@ -19,15 +28,88 @@
 */
 struct VSid : Module {
 	enum ParamIds {
-		FREQ,
+		FREQ1,
+		FREQ2,
+		FREQ3,
+		FREQ1_ATT,
+		FREQ2_ATT,
+		FREQ3_ATT,
+		OCT1,
+		OCT2,
+		OCT3,
+		PW1,
+		PW2,
+		PW3,
+		PW1_ATT,
+		PW2_ATT,
+		PW3_ATT,
+		ENV1_A,
+		ENV1_D,
+		ENV1_S,
+		ENV1_R,
+		ENV2_A,
+		ENV2_D,
+		ENV2_S,
+		ENV2_R,
+		ENV3_A,
+		ENV3_D,
+		ENV3_S,
+		ENV3_R,
+		WAVE1_NOISE,
+		WAVE1_PULSE,
+		WAVE1_SAW,
+		WAVE1_TRI,
+		WAVE1_RING,
+		WAVE1_SYNC,
+		WAVE2_NOISE,
+		WAVE2_PULSE,
+		WAVE2_SAW,
+		WAVE2_TRI,
+		WAVE2_RING,
+		WAVE2_SYNC,
+		WAVE3_NOISE,
+		WAVE3_PULSE,
+		WAVE3_SAW,
+		WAVE3_TRI,
+		WAVE3_RING,
+		WAVE3_SYNC,
+		FILT1,
+		FILT2,
+		FILT3,
+		FILT_EXT,
+		FILT_HP,
+		FILT_BP,
+		FILT_LP,
+		FILT_CUTOFF,
+		FILT_CUTOFF_ATT,
+		FILT_RES,
+		FILT_RES_ATT,
 		WAVEFORM,
 		CUTOFF,
+		MUTE3,
 		NUM_PARAMS
 	};
 	enum InputIds {
+		GATE1,
+		GATE2,
+		GATE3,
+		FREQ_CV1,
+		FREQ_CV2,
+		FREQ_CV3,
+		PW1_CV,
+		PW2_CV,
+		PW3_CV,
+		SYNC1,
+		SYNC2,
+		SYNC3,
+		FILT_CUTOFF_CV,
+		FILT_RES_CV,
+		MUTE3_CV,
+		EXT_AUDIO,
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		ENV3_OUT,
 		OSC3_OUT,
 		AUDIO_OUT,
 		NUM_OUTPUTS
@@ -71,7 +153,7 @@ void VSid::step() {
 
 	for(int base = 0; base <= 0x0e; base+=7 ) {
 		sid.write(base+0,0); // freq
-		sid.write(base+1, params[FREQ].value);
+		sid.write(base+1, params[FREQ1].value);
 		sid.write(base+4, int(params[WAVEFORM].value)| (clock++ % 100000 == 0 ? 1 : 0)); // control register
 	}
 
@@ -113,7 +195,7 @@ VSidWidget::VSidWidget(VSid *module) : ModuleWidget(module) {
 	addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
 	addChild(Widget::create<ScrewSilver>(Vec(box.size.x-30, 365)));
 
-	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, RACK_GRID_WIDTH), module, VSid::FREQ, 0, 255, 0));
+	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, RACK_GRID_WIDTH), module, VSid::FREQ1, 0, 255, 0));
 	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, 3*RACK_GRID_WIDTH), module, VSid::WAVEFORM, 0, 255, 0));
 
 	addParam(ParamWidget::create<RoundSmallBlackKnob>(Vec(RACK_GRID_WIDTH, 5*RACK_GRID_WIDTH), module, VSid::CUTOFF, 0, 255, 0));
